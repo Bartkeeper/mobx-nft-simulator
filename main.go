50fd65d19f74353b66200285d38bca88932c8baf
mscdr <martin.schaeffner@datarella.com>
2022-06-03 09:40:54 +0200

    added a function to enter NFTs manually. Need to work on changing the data set

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/csv"
 	"fmt"
 	"math"
+	"math/rand"
 	"os"
 	"strconv"
 )
@@ -35,15 +36,25 @@ type nftSet []nfTier
 
 var totalMilesWeight float64
 
+type nftMap struct {
+	tier       float64
+	multiplier float64
+	amount     float64
+}
+
+type mapping []nftMap
+
 func main() {
 
-	userGroup := importCSV()
-	userGroup2 := importCSV()
+	// userGroup := importCSV()
+	// userGroup2 := importCSV()
 
-	userGroup2.resetNFT()
-	userGroup.calculateRewards()
-	userGroup2.calculateRewards()
-	userGroup.calculateNFTbonus(userGroup2)
+	// userGroup2.resetNFT()
+	// userGroup.calculateRewards()
+	// userGroup2.calculateRewards()
+	// userGroup.calculateNFTbonus(userGroup2)
+
+	setNFTProps(mapping{})
 
 }
 
@@ -244,4 +255,60 @@ func importCSV() userSet {
 
 	}
 	return csvUserGroup
+}
+
+func setNFTProps(m mapping) mapping {
+	fmt.Println("How many different tiers do you want? (excluding non-NFT holders)")
+
+	// reader := bufio.NewReader(os.Stdin)
+
+	var f4 float64
+	_, _ = fmt.Scanf("%f4", &f4)
+	fmt.Println(f4)
+
+	numberOfNFTs := int(f4)
+
+	for j := 0; j < numberOfNFTs; j++ {
+
+		var f float64
+		var f2 float64
+		var f3 float64
+
+		fmt.Println("For Nft Tier", j, ", please enter the tier identifier as a number")
+		_, _ = fmt.Scanf("%f", &f)
+
+		fmt.Println("For NFT Tier", j, ", please enter the multiplier as a number")
+		_, _ = fmt.Scanf("%f2", &f2)
+
+		fmt.Println("For Nft Tier", j, ", please enter the issued NFT amount")
+		_, _ = fmt.Scanf("%f3", &f3)
+
+		tierF := float64(f)
+		multiplierF := float64(f2)
+		amountF := float64(f3)
+
+		m2 := nftMap{tierF, multiplierF, amountF}
+
+		m = append(m, m2)
+	}
+	fmt.Println(m)
+	return m
+}
+
+func (us userSet) manipulateUsers(m mapping) {
+
+	// toDo: restructure for loop to loop over the usetSet only for the number of NFTamounts
+
+	for _, nftTier := range m {
+
+		am := int(nftTier.amount)
+		amounts := rand.Intn(am)
+
+		for amounts, mobxUser := range us {
+			mobxUser.nftWeight = nftTier.multiplier
+			// fmt.Println("The mobx rewards of user", i, "is", mobxUser.nftWeight)
+			us[amounts].nftWeight = mobxUser.nftWeight
+		}
+	}
+
 }
